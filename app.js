1760485e1f75474a80cf145e592ac458cc23d2bc
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-19 09:29:49 +0530

    added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const { products } = require("./data");
+const { log } = require("console");
 
 const app = express();
 
@@ -9,12 +10,27 @@ app.get("/", (req, res) => {
   // res.json(products);
 });
 
+// sending some data
 app.get("/api/products", (req, res) => {
   const newProducts = products.map((p) => {
     const { id, name, image } = p;
     return { id, name, image };
   });
   res.json(newProducts);
+});
+
+// sending single data
+// app.get("/api/products/1", (req, res) => {
+//   const singleData = products.find((p) => p.id === 1);
+//   res.json(singleData);
+// });
+
+// sending data dynamically
+app.get("/api/products/:productId", (req, res) => {
+  // log(req.params);
+  const { productId } = req.params;
+  const singleData = products.find((p) => p.id === +productId);
+  res.json(singleData);
 });
 
 app.listen(5000, () => {
