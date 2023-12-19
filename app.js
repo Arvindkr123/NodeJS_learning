f6850a0d0b8b24c7d3b2eace078dd120ea1fa2fd
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-19 15:08:10 +0530

    added method delete and put

--- app.js
+++ app.js
@@ -18,7 +18,51 @@ app.post("/api/people", (req, res) => {
       .status(400)
       .json({ success: false, msg: "please provide a name" });
   }
-  res.status(201).json({success:true, person:name})
+  res.status(201).json({ success: true, person: name });
+});
+
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide a name" });
+  }
+  res
+    .status(201)
+    .json({ success: true, data: [...people, { id: Date.now(), name: name }] });
+});
+
+// Put Method
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const person = people.find((p) => p.id === Number(id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with this ${id}` });
+  }
+  const newPeople = people.map((p) => {
+    if (p.id === Number(id)) {
+      p.name = name;
+    }
+    return p;
+  });
+  res.status(202).json({ success: true, data: newPeople });
+});
+
+// delete method
+app.delete("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const person = people.find((p) => p.id === Number(id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `No person with id ${id}` });
+  }
+  const newPeople = people.filter((p) => p.id !== +person.id);
+  res.status(202).json({ success: true, data: newPeople });
 });
 
 app.post("/login", (req, res) => {
