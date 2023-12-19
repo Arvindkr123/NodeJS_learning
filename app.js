954da3815e1d85cf6f410b0665947c183fe86e2e
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-19 13:41:10 +0530

    added post

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ let { people } = require("./data");
 
 app.use(express.static("./express-tutorial/04-methods-public"));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // read data
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide a name" });
+  }
+  res.status(201).json({success:true, person:name})
 });
 
 app.post("/login", (req, res) => {
